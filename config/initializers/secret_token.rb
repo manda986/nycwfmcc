561ca22b59457c3a75182a8ea8de8fba091e77df
Amanda Li <manda986@gmail.com>
2013-10-01 22:59:58 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nycwfmcc::Application.config.secret_key_base = '6cfd0dfa90ccecf97023228f51e2248f308fec3af1c4616a140a4687078358b7dcbaeb9c3269f18057b5fd60294338827546ce6f413f372e0aec4aaf0e294562'
